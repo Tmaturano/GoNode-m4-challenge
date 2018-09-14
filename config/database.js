a50c191464c0354712a6cbe0b11b7bf9ad986293
Thiago Maturana <thiagomatu@hotmail.com>
2018-09-14 19:27:55 -0300

    setting up postgres for adonis

--- config/database.js
+++ config/database.js
@@ -70,11 +70,11 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', ''),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
+      user: Env.get('DB_USER', ''),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
+      database: Env.get('DB_DATABASE', '')
     },
     debug: Env.get('DB_DEBUG', false)
   }
